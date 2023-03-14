939a9c1b0d1683c070b93a99c63b30c1afb954de
eventtezukayama <127727617+eventtezukayama@users.noreply.github.com>
2023-03-14 19:02:14 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,5 @@ window.addEventListener("DOMContentLoaded", function () {
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
     document.getElementById("title").style.transform = "transelateX(0)"
+  });
 });
