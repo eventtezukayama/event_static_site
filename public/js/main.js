7222a7db9925bfca51c50166838227be38c9dbbe
eventtezukayama <127727617+eventtezukayama@users.noreply.github.com>
2023-03-14 19:01:43 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
-    document.getElementById("title").style.transform = "transelateX"
+    document.getElementById("title").style.transform = "transelateX(0)"
 });
